aa35a6a920dbaf9fa3a0788bd5e8eb9a727a5dbc
Lorelien Michiels <r0894804@student.thomasmore.be>
2026-01-02 12:48:11 +0100

    feat: add Databse class

---
+++ classes/Database.php
@@ -0,0 +1,25 @@
+<?php
+
+class Database
+{
+    private string $host = 'localhost';
+    private string $dbName = 'boekhandel';
+    private string $user = 'root';
+    private string $password = '';
+    private ?PDO $connection = null;
+
+    public function getConnection(): PDO
+    {
+        if ($this->connection === null) {
+            $dsn = "mysql:host={$this->host};dbname={$this->dbName};charset=utf8mb4";
+            $options = [
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            ];
+
+            $this->connection = new PDO($dsn, $this->user, $this->password, $options);
+        }
+
+        return $this->connection;
+    }
+}
