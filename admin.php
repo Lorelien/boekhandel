537fd95cc9e1fe4a28be17251074ec3a71d81d41
Lorelien Michiels <r0894804@student.thomasmore.be>
2026-01-03 11:56:02 +0100

    feat: linking user class to admin and register

--- admin.php
+++ admin.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/classes/Book.php';
 require_once __DIR__ . '/classes/Category.php';
 require_once __DIR__ . '/classes/Author.php';
 require_once __DIR__ . '/classes/Publisher.php';
+require_once __DIR__ . '/classes/User.php'; 
 
 $pdo = $db->getConnection();
 
